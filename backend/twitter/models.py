4ee35aba35f88a71b20006284a33d2e79d0df83d
Alexander Budko <103473998+alex-budko@users.noreply.github.com>
2023-01-15 20:47:58 -0500

    twitter model implementration

--- backend/twitter/models.py
+++ backend/twitter/models.py
@@ -1,2 +1,80 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
+from django.contrib.humanize.templatetags import humanize
 
+class UserAccountManager(BaseUserManager):
+    def create(self, username, password=None):
+
+        user = self.model(
+            username=username,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, username, password=None):
+
+        user = self.model(
+            username=username,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, username, password=None):
+        user = self.create_user(
+            password=password,
+            username=username,
+        )
+
+        user.is_admin = True
+        user.save(using=self._db)
+
+        return user
+
+class UserAccount(AbstractBaseUser):
+
+    username = models.CharField(
+        max_length=265, primary_key=True, unique=True, blank=False)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['username']
+
+    def has_perm(self):
+        "Does the user have a specific permission?"
+        return True
+
+    def has_module_perms(self):
+        "Does the user have permissions to view the app `app_label`?"
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        return self.is_admin
+
+    def get_username(self):
+        return self.username
+
+    def __str__(self):
+        return self.email
+
+
+class Tweet(models.Model):
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True, auto_created = True, blank=False)
+    text = models.CharField(max_length=280)
+
+    def created_at(self):
+        return humanize.naturaltime(self.created)
